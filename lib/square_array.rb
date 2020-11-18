6827a521176cad6a8dfd5aaaffdcd3ee9b57664a
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-18 18:13:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ array = [1, 2, 3, 4, 5]
 
 def square_array(array)
 while array.length.times do
-  new_array.push(array[index] ** index )
+  puts new_array.push(array[index] ** index )
 end
 
 new_array
